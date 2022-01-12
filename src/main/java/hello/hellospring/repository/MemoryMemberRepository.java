0ee18ef6226ae6ae930f4974dc8624614785e8b5
JeonDoHyun <52389219+JeonDoHyun@users.noreply.github.com>
2022-01-12 23:22:37 +0900

    SpringConfig 클래스를 이용한 빈 등록 (인프런 김영한님 강의)

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
+
 public class MemoryMemberRepository implements MemberRepository {
 
     //회원 저장할때 어디 해야할것 아님
