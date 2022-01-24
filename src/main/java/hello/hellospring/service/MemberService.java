e81183e60f8943663fefc7e92b1a3745007a222e
JeonDoHyun <52389219+JeonDoHyun@users.noreply.github.com>
2022-01-25 00:15:46 +0900

    JPA 적용! (인프런 김영한님 강의)

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,6 +5,7 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,6 +24,7 @@ public class MemberService {
     }
 
     //회원가입
+    @Transactional
     public Long join(Member member) {
         //같은 이름이 있는 중복회원 x
         Optional<Member> result = memberRepository.findByName(member.getName());
